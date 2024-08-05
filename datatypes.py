f5c403fd42c130501fe337105b0adf5b034844af
mohamedabdikadir7 <mohaabdikadiir@gmail.com>
2024-08-05 17:24:37 +0300

    python-data_types

---
+++ datatypes.py
@@ -0,0 +1,32 @@
+## 1 list 
+## A list in Python is an ordered, mutable collection of elements
+## they are like arrays in js 
+fruits = ["apple" , "mango" ,"banana" , "cherry" , "pineapple"]
+print(fruits)
+
+## accesing list 
+print(fruits[0])  # Output: apple
+
+print(fruits[1:2])
+## Modifying Elements:
+fruits[1] = 'blueberry'
+print(fruits)
+
+## 2 tuples
+## Tuples are nearly identical to lists, with two key differences:
+
+# First, tuples are created with open and close parentheses (in place of square brackets).
+my_tuples = ("garissa" , "nairobi" , "naks")
+print(my_tuples)
+
+## sets
+my_food = {"pilau" , 'mahamri' , "nazi"}
+print(my_food)
+
+## dict
+## dict in js is same as js objects
+my_dict = {'name': 'Alice', 'age': 25}
+print(my_dict)
+my_tuple = (1, 2, 3, 4, 5)
+print("Tuple elements:", my_tuple)
+

d97b466a7a36677f33bec310e3b164777f69fd80
mohamedabdikadir7 <mohaabdikadiir@gmail.com>
2024-08-09 23:24:02 +0300

    python range

---
+++ range.py
@@ -0,0 +1,18 @@
+## Ranges are a very simple type of sequence that is most commonly used in for loops.
+## we use range() constructor
+
+for n in range(5):
+    print(n)
+## out put => 0,1,2,3,4
+my_range = range(4)
+print(my_range)
+# => range(0, 4)
+
+rage = range(7)
+print(rage)
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+print(thisdict)

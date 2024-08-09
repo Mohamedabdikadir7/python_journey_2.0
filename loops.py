c3fd40153bc129ccb3c573a911c83038692062cf
mohamedabdikadir7 <mohaabdikadiir@gmail.com>
2024-08-09 23:25:07 +0300

    python loops

--- loops.py
+++ loops.py
@@ -1,34 +1,41 @@
-## A for loop is used to iterate over a sequence (like a list, tuple, dictionary, set, or string) and execute a block of code for each item in the sequence.
-fruits = ["apple" , "mango", "bananna"]
-for fruit in fruits:
-    print(fruit)
+# ## A for loop is used to iterate over a sequence (like a list, tuple, dictionary, set, or string) and execute a block of code for each item in the sequence.
+# fruits = ["apple" , "mango", "bananna"]
+# for fruit in fruits:
+#     print(fruit)
 
-list_of_lists = [ [1, 2, 3], [4, 5, 6], [7, 8, 9]]
-for list in list_of_lists:
+# list_of_lists = [ [1, 2, 3], [4, 5, 6], [7, 8, 9]]
+# for list in list_of_lists:
    
-        print(list)
-## while loop
-i = 0
-while i < 5:
-      print('hello,wolrd')
-      i += 1
-# for loop
-# iterate from i = 0 to i = 3
-for i in range(4):
-    print(i)
+#         print(list)
+# ## while loop
+# i = 0
+# while i < 5:
+#       print('hello,wolrd')
+#       i += 1
+# # for loop
+# # iterate from i = 0 to i = 3
+# for i in range(4):
+#     print(i)
 
-digits = [0, 1, 5]
+# digits = [0, 1, 5]
 
-for i in digits:
-    print(i)
-else:
-    print("No items left.")
+# for i in digits:
+#     print(i)
+# else:
+#     print("No items left.")
 
-for x in "banana":
-  print(x)
+# for x in "banana":
+#   print(x)
 
-for y in range(7):
-     print('hello world')
+# for y in range(7):
+#      print('hello world')
 
-for x in range(2, 30):
-  print(x)
+# for x in range(2, 30):
+#   print(x)
+def spam():
+ eggs = 'spam local'
+ print(eggs)
+
+ cat = ["jiko", "kilo" , "polo"]
+ cat.insert(1, "riko")
+ print(cat)

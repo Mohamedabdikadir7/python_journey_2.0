5c9eded4a04bfb0fa69b305922a3612b3f5f4010
mohamedabdikadir7 <mohaabdikadiir@gmail.com>
2024-08-14 22:24:06 +0300

    oop python

---
+++ oop1.py
@@ -0,0 +1,27 @@
+"""Class
+Definition: A class is a blueprint or template for creating objects. It defines attributes (data) and methods (functions) that the objects created from the class will have."""
+
+# class Employee:
+#     def __init__(self, name, age, race, salary, emp_id):
+#         self.name = name
+#         self.age = age
+#         self.race = race
+#         self.salary = salary
+#         self.emp_id = emp_id
+class Employee:
+    def __init__(self, name, age, race, salary, emp_id):
+        self.name = name
+        self.age = age
+        self.race = race
+        self.salary = salary
+        self.emp_id = emp_id
+
+    def give_raise(self, amount):
+        self.salary += amount
+        print(f"{self.name} received a raise. New salary: ${self.salary}")
+
+# Creating an instance of Employee
+employee3 = Employee("Alice Brown", 40, "White", 60000, "E003")
+
+# Using the give_raise method
+employee3.give_raise(5000)  # Output: Alice Brown received a raise. New salary: $65000

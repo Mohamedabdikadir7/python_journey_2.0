1681c6c8009a6b67852b3fdb753bec64e90889ea
mohamedabdikadir7 <mohaabdikadiir@gmail.com>
2024-08-21 23:11:36 +0300

    learned import topic

---
+++ import.py
@@ -0,0 +1,13 @@
+## Importing an Entire Module:
+import lists 
+lists.fruits
+## Importing a Specific Function or Variable:
+from lists import fruits
+print(fruits)
+import os
+
+
+current_directory = os.getcwd()
+
+
+print("Current Working Directory:", current_directory)
